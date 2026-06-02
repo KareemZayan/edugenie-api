d2973115a6a22aa6b5a61789390bbc119558bb41
fatima7Mohamed <ffatmammohamed110@gmail.com>
2026-06-02 22:22:31 +0300

    Setup Optimized Course Entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { CoursesModule } from './courses/courses.module';
 
 @Module({
   imports: [
@@ -20,6 +21,7 @@ import { UsersModule } from './users/users.module';
     }),
     AuthModule,
     UsersModule,
+    CoursesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/courses/dto/create-course.dto.ts
@@ -0,0 +1,41 @@
+import { IsString, IsNumber, IsEnum, IsArray, IsMongoId, IsOptional, Min, MinLength, IsNotEmpty } from 'class-validator';
+import { CourseLevel } from '../enums/level.enum';
+
+export class CreateCourseDto {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(5, { message: 'Title is too short' })
+    title!: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(20, { message: 'Description must be detailed' })
+    description!: string;
+
+    @IsNumber()
+    @Min(0)
+    price!: number;
+
+    @IsString()
+
+    thumbnail!: string;
+
+    @IsEnum(CourseLevel)
+    level!: CourseLevel;
+
+    @IsMongoId({ message: 'Instructor must be a valid Mongo ID' })
+    instructorId!: string;
+
+    @IsMongoId({ message: 'Category must be a valid Mongo ID' })
+    categoryId!: string;
+
+    @IsOptional()
+    @IsArray()
+    @IsString({ each: true })
+    goals?: string[];
+
+    @IsOptional()
+    @IsArray()
+    @IsString({ each: true })
+    requirements?: string[];
+}
